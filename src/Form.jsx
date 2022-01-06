d3ed548be4fe67a7c9784b872bad018a1792e437
Andrew E <45772398+DrewEvans@users.noreply.github.com>
2022-01-06 16:24:01 +0100

    merge

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 function Form({ children, title }) {
 	const renderChildren = (child) => <>{child}</>;
 
@@ -10,13 +9,6 @@ function Form({ children, title }) {
 			{renderChildren(children)}
 		</form>
 	);
-=======
-function Form({children, renderChildren}) {
-	console.log()
-	console.log(children)
-	
-	return <div><h1></h1>{renderChildren(children)}</div>;
->>>>>>> 9ea5e82235b288c94bc0c5be5c6f5e6a9df4427c
-}
+};
 
 export default Form;
